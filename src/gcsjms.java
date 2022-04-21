bc800623fc3b07a210410bc711be2f4b2614d306
f <f@>
2022-04-21 19:49:29 +0800

    工厂模式

---
+++ src/gcsjms.java
@@ -0,0 +1,47 @@
+public class gcsjms {
+    public static void main(String[] args){
+    Factory factoryA = new FactoryA();
+    Product productA = factoryA.creatProduct();
+    productA.info();
+
+    Factory factoryB = new FactoryB();
+    Product productB = factoryB.creatProduct();
+    productB.info();
+
+
+    }
+}
+
+    interface  Factory {
+public Product creatProduct();
+    }
+    interface Product{
+        public void info();
+    }
+    class FactoryA implements Factory{
+        @Override
+        public Product creatProduct(){
+            return  new ProductA();
+        }
+    }
+
+    class FactoryB implements Factory{
+        @Override
+        public Product creatProduct(){
+            return  new ProductB();
+        }
+    }
+    class ProductA implements Product{
+        @Override
+        public  void info(){
+            System.out.println("产品的信息：A");
+        }
+    }
+
+    class ProductB implements Product{
+        @Override
+        public void info(){
+            System.out.println("产品的信息：B");
+        }
+    }
+
